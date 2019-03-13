10b729cfe9b7fbd04cda7bc92ea46e567e6c448e
salmameniawy <salmamostafameniawy@gmail.com>
2019-03-14 00:34:39 +0200

    first commit

--- opensource_39/app/Http/Controllers/PostsController.php
+++ opensource_39/app/Http/Controllers/PostsController.php
@@ -9,8 +9,6 @@ class PostsController extends Controller
 {
     public function index()
     {
-       
-        
         return view('posts.index',[
             'posts'=> Post::all()
         ]);
